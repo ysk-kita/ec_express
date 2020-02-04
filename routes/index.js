c032a112f94b195d46e79bda20481a51e22e0f5b
Ubuntu <ubuntu@ip-199-61-129-10.ap-northeast-1.compute.internal>
2020-02-04 09:42:36 +0000

    top page

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,45 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  var price = 2000;
+  price = price.toLocaleString('ja-JP', {"style":"currency", "currency":"JPY"})
+    
+  var data = { 
+    title: 'Express' ,
+    items: [
+      {
+        'name': 'hoge',
+        'img_alt': 'test01',
+        'img': '',
+        'price': "1000".toLocaleString('ja-JP', {"style":"currency", "currency":"JPY"})
+      },
+      {
+        'name': 'fuga',
+        'img_alt': 'test02',
+        'img': '',
+        'price': price
+      },
+      {
+        'name': 'fuga',
+        'img_alt': 'test03',
+        'img': '',
+        'price': price
+      },
+      {
+        'name': 'fuga',
+        'img_alt': 'test04',
+        'img': '',
+        'price': price
+      },
+      {
+        'name': 'fuga',
+        'img_alt': 'test05',
+        'img': '',
+        'price': price
+      }
+    ]  
+  };
+  res.render('index', data);
 });
 
 module.exports = router;
