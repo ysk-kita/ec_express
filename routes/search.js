e472533c14c1a5f14053aad2313087f4d3c1fd9f
Ubuntu <ubuntu@ip-199-61-129-10.ap-northeast-1.compute.internal>
2020-02-05 07:50:49 +0000

    modules add

---
+++ routes/search.js
@@ -0,0 +1,37 @@
+var express = require('express');
+var router = express.Router();
+
+// 自作モジュール呼び出し
+var items = require('../modules/items.js')
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  var price = 2000;
+  price = price.toLocaleString();
+  var isSale = true;
+  
+  // Knexで色々書いてDB取得する
+  items.sayHello();
+  
+  var data = {
+    title: 'Kitazon' ,
+    sale_status: isSale ? "active": "disabled",
+    items: [
+      {
+        'name': 'カップラーメン',
+        'img_alt': 'img',
+        'img': 'images/item03.png',
+        'price': price
+      },
+      {
+        'name': '豚骨ラーメン',
+        'img_alt': 'img',
+        'img': 'images/item04.png',
+        'price': price
+      },
+    ]
+  };
+  res.render('index', data);
+});
+
+module.exports = router;
